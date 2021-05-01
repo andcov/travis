b0905ece162e1057aa38fbf43882d7c802d69abe
andcov <andreicovaci123@gmail.com>
2021-05-01 22:31:38 +0300

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+fn add_two(x: i32) -> i32 {
+    x + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_add_two() {
+        assert_eq!(add_two(4), 6);
+    }
+}
