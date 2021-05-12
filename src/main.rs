2f272677aad6876d17291dc666b975c9dfb278dc
andcov <andreicovaci123@gmail.com>
2021-05-12 10:53:34 +0300

    Change smthing

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@ use travis_test::add_two;
 
 fn main() {
     let a = 4u32;
-    hello(a);
+    println!("{}", hello(a));
     if a > 0 {
-	    println!("{}", add_two(1));
-	}
+  println!("{}", add_two(1));
+    }
 }
 
 fn hello(a: u32) -> bool {
-	if a > 0 {
-		return true;
-	}
-	false
+    if a > 0 {
+        return true;}
+    false
 }
